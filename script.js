211bbc9ac166db172ec0c38061cec70a31f543ce
Bret Zawilski <bret.zawilski@gmail.com>
2021-05-06 22:40:07 +0100

    Rough JS in place

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Linked');
+const displayStory = () => {
+  const para = document.createElement('p');
+  container.appendChild(para);
+  para.innerHTML = `Test <span>test</span> test. So then what do I need to do? I need to have some objects that I can pass into this thing so that it's a fair example of how a story would work.`;
+}
+
+const container = document.getElementById('container');
+
+
+displayStory();
